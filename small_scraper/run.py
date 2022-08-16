d569433327baf20677bc63659be9029d813a9749
wegar <artur.wegrzyn@axpo.pl>
2022-08-16 16:30:17 +0200

    yet another fix...

--- small_scraper/run.py
+++ small_scraper/run.py
@@ -11,6 +11,32 @@ my_env = os.environ["TEST_ENV_VAR"]
 print("my_env: ", my_env)
 
 
+print("loading environment vars needed to connect to GCP...")
+env_type = os.environ["ENV_TYPE"]
+env_project_id = os.environ["ENV_PROJECT_ID"]
+env_private_key_id = os.environ["ENV_PRIVATE_KEY_ID"]
+env_private_key = os.environ["ENV_PRIVATE_KEY"]
+env_client_email = os.environ["ENV_CLIENT_EMAIL"]
+env_client_id = os.environ["ENV_CLIENT_ID"]
+env_auth_uri = os.environ["ENV_AUTH_URI"]
+env_token_uri = os.environ["ENV_TOKEN_URI"]
+env_auth_provider = os.environ["ENV_AUTH_PROVIDER"]
+env_client_cert = os.environ["ENV_CLIENT_CERT"]
+
+
+my_dict = {
+  "type": env_type,
+  "project_id": env_project_id,
+  "private_key_id": env_private_key_id,
+  "private_key": env_private_key,
+  "client_email": env_client_email,
+  "client_id": env_client_id,
+  "auth_uri": env_auth_uri,
+  "token_uri": env_token_uri,
+  "auth_provider_x509_cert_url": env_auth_provider,
+  "client_x509_cert_url": env_client_cert
+}
+
 print("creating data table...")
 df = pd.DataFrame(
     data={
@@ -20,10 +46,8 @@ df = pd.DataFrame(
 )
 
 
-print("creating data table...")
-str_key = os.environ["MY_SECRET_KEY"]
-info = json.loads(str_key)
-sa_credentials = service_account.Credentials.from_service_account_info(info)
+print("creating credentials...")
+sa_credentials = service_account.Credentials.from_service_account_info(my_dict)
 
 print("uploading data table...")
 df.to_gbq(
